f52b833596a9273b45a7dbfd09ccfc5b5df75c80
Zdes <fedoroveo1@mail.ru>
2021-05-12 00:36:10 +0300

    commit8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,22 @@
-import java.util.ArrayList;
-import java.util.Collection;
-
 public class Main {
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
     public static void main(String[] args) {
-        Collection<Car> cars = new ArrayList<>();
-        for(int i = 0; i < 10; i++) {
-            cars.add(new Car("BMW" + i, i));
-        }
-        for (Car car : cars) {
-            System.out.println(car.getModel() + " " + car.getNumber());
-        }
+        //Collection<Car> cars = new ArrayList<>(); // добавление элементов по порядку
+        //Collection <Car> cars = new HashSet<>(); // хаотично
+        //Collection <Car> cars = new LinkedList<>(); // по порядку
+        //Collection <Car> cars = new LinkedHashSet<>(); // по порядку
+        //Map <String, Car> carHashMap = new HashMap<>();
+//        Set<Integer> carSet = new TreeSet<>(new Comparator<Integer>() {
+
+
     }
 }
